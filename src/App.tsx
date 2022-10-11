eb4487cfbb8afa43e8d578199386d65592157f4f
yudhayepe <syudhaprakosa@gmail.com>
2022-10-11 18:33:19 +0700

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <>
       <nav>
-        <ul className='flex bg-slate-600 text-white p-4'>
+        <ul className='flex justify-end bg-slate-600 text-white p-4'>
           <li>Instagram</li>
           <li>Telegram</li>
           <li>Facebook</li>
